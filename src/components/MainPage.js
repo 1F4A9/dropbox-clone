9a61636d1b826f1bdcac485c5f14c90807986f48
Kevin Lu <kevinl_91@hotmail.com>
2020-02-21 10:17:05 +0100

    changed main

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,17 +1,24 @@
 import React from 'react';
-
 import { fetchDataFromUser } from '../api/API';
+import SideBar from '../components/SideBar';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
 
 function MainPage() {
 
   // fetchDataFromUser(); // denna funktionen console loggar användarens filer och data.
 
   return (
-    <>
+    <Container>
+      <SideBar />
       <h1>MAIN PAAAAGE</h1>
       <h2>TODO: Router system för login/main</h2>
-    </>
+
+    </Container>
   )
 }
 
-export default MainPage; 
+export default MainPage;
