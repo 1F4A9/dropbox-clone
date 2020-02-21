8770634b8fbbcaeff40372fba82065e5d495db85
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-02-21 14:13:32 +0100

    done some sidebar styling

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -3,12 +3,22 @@ import styled from 'styled-components';
 
 import { updateToken } from '../Observables/Store';
 
-const Button = styled.button`
+const Container = styled.div`
+  cursor: pointer;
+  font-size: 22px;
 
+  :hover {
+    text-decoration: underline;
+  }
 `;
 
 const LogoutButton = () => {
-  return <Button onClick={() => updateToken(null)}/>
+  return (
+      <Container onClick={() => updateToken(null)}>
+        Logout
+      </Container>
+    )
+
 }
 
 export default LogoutButton;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,28 +1,45 @@
 import React from 'react'
 import styled from 'styled-components';
 
+import LogoutButton from './LogoutButton';
+
 const Container = styled.aside`
   display: flex;
-  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 2;
+  height: 100vh;
+  max-width: 300px;
+  position: sticky;
+  top: 0;
+  left: 0;
+  background-color: #f7f9fa;
 
-  .box {
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
-  h1 {
-    color: ${props => props.color}
+  .footer {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 50px;
   }
 `;
 
 
 function SideBar() {
   return (
-    <Container color={'blue'}>
-      <div className="box">
+    <Container>
+      <div className="header">
         <h1>Home</h1>
         <h1>Upload files</h1>
         <h1>New folder</h1>
       </div>
-      <div className="box-left"></div>
+      <div className="footer">
+        <LogoutButton />
+      </div>
     </Container>
   )
 }
