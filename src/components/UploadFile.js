ec54c00b43e948eb631a25e470db598338b13e5a
Kevin Lu <kevinl_91@hotmail.com>
2020-02-23 17:28:35 +0100

    update upload

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -4,16 +4,41 @@ import { token$ } from '../Observables/Store';
 
 function UploadFile(props){
   const [token, updateTokenState] = useState(token$.value);
-  const [file, updatefile] = useState([]);
+  const [file, updateFile] = useState(null);
 
-  const handleUpload= (files) =>{
+  const path = window.location.pathname;
+  console.log(path)
+
+  const handleUpload= (e) =>{
+    updateFile(e.target.files[0]);
+    console.log(file)
+
     const dropbox = new Dropbox({ accessToken: token$, fetch });
-
-    console.log(dropbox.filesUpload)
+    dropbox.filesUpload({
+      contents: file,
+      path: path,
+      autorename: true,
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    
   }
 
   return (
-    <p>Upload File</p>
+    <>
+      <label htmlFor='uploadFile' >Upload File</label>
+      <input
+        id='uploadFile'
+        type='file'
+        multiple
+        onChange={handleUpload}
+        style={{visibility:'hidden'}}
+      />
+    </>
   )
 }
 
