46049d65bd7b448b14cbf44f3d94d386cc10a08b
Daniel Reineborn <d.reineborn@gmail.com>
2020-02-26 12:23:03 +0100

    added index.js to utilities

---
+++ src/utilities/index.js
@@ -0,0 +1,22 @@
+import { PATH_BASENAME } from "../constants/constants";
+
+export function stringToArrBreadCrumbs(str) {
+  str = PATH_BASENAME + str; //Gjort en konstant som kan användas för path basename.
+  let arr = str.split("/");
+
+  return arr;
+}
+
+export function returnRightPath(str, path) {
+  let arr = stringToArrBreadCrumbs(path);
+
+  let index = arr.indexOf(str);
+  let uncutPath = arr.slice(0, index + 1).join("/");
+  let regEx = /home/i;
+  let returnPath = uncutPath.replace(regEx, "");
+  return returnPath;
+}
+
+export function getCurrentPathName() {
+  return window.location.pathname.substring(5);
+}
